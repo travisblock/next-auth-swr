304046ca1e44eb478621660807a51285927640e2
ajid2 <ajidalmajid6@gmail.com>
2021-11-03 17:52:38 +0700

    Boilerplate
    Signed-off-by: ajid2 <ajidalmajid6@gmail.com>

---
+++ src/pages/user/task/create.jsx
@@ -0,0 +1,7 @@
+export default function TaskCreate() {
+    return (
+        <div>
+            <h1>Create Task</h1>
+        </div>
+    )
+}
---
+++ src/pages/user/task/index.jsx
@@ -0,0 +1,7 @@
+export default function Task() {
+    return (
+        <div>
+            <h1>Task</h1>
+        </div>
+    );
+}
