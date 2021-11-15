4997a5a2cb2426ec8fa6078b4bb7cf315d5e993d
ajid2 <ajidalmajid6@gmail.com>
2021-11-15 11:45:32 +0700

    Added shallow routing
    Signed-off-by: ajid2 <ajidalmajid6@gmail.com>

--- src/components/sidebar/home.jsx
+++ src/components/sidebar/home.jsx
@@ -26,7 +26,7 @@ export default function HomeSidebar({ collapsed } = {}) {
         <div className={`${style.sidebar} ${collapsed ? style.collapse : null}`}>
             <ul className={style.sidebar_ul}>
                 <li className={style.sidebar_ul_li}>
-                    <Link href={route('user.index')}>
+                    <Link href={route('user.index')} shallow={true}>
                         <a className={`${style.sidebar_a}`}>
                             <span className={style.icon}>
                                 <svg className={style.svg} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
@@ -36,7 +36,7 @@ export default function HomeSidebar({ collapsed } = {}) {
                     </Link>
                 </li>
                 <li className={style.sidebar_ul_li}>
-                    <Link href={route('user.task.index')}>
+                    <Link href={route('user.task.index')} shallow={true}>
                         <a className={`${style.sidebar_a}`}>
                             <span className={style.icon}>
                             <svg className={style.svg} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
