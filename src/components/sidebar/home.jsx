326067c8d8d5c509baeeda9d50a39c6a28ec3342
ajid2 <ajidalmajid6@gmail.com>
2021-11-16 13:35:14 +0700

    Sidebar: fix active menu
    Signed-off-by: ajid2 <ajidalmajid6@gmail.com>

--- src/components/sidebar/home.jsx
+++ src/components/sidebar/home.jsx
@@ -5,13 +5,11 @@ import { route } from "config/routes";
 import Link from "next/link"
 import { useState } from "react";
 import { sessionOptions } from "lib/session";
-import { useClientRouter } from "use-client-router";
+import { useRouter } from "next/router";
 
 export default function HomeSidebar({ collapsed } = {}) {
-    const router = useClientRouter();
+    const router = useRouter();
     const [islogout, setIslogout] = useState(false)
-
-    console.log(router.pathname);
 
     async function logout(e) {
         e.preventDefault();
@@ -33,7 +31,7 @@ export default function HomeSidebar({ collapsed } = {}) {
             <ul className={style.sidebar_ul}>
                 <li className={style.sidebar_ul_li}>
                     <Link href={route('user.index')} shallow={true}>
-                        <a className={`${style.sidebar_a} ${router.pathname == route('user.index') ? style.active : null}`}>
+                        <a className={`${style.sidebar_a} ${router.pathname == route('user.index') ? style.active : ''}`}>
                             <span className={style.icon}>
                                 <svg className={style.svg} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                             </span>
